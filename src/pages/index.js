19b7d2c69b67763f8d89ebb8ce1be7917230af04
aroxu <30624945+aroxu@users.noreply.github.com>
2023-07-27 00:54:04 +0900

    :iphone: Improve responsible design
    - TODO: Add screenshot to right side with [SwiperJS](https://swiperjs.com/get-started)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,21 @@
-import React from 'react';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
+import React from "react";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={styles.heroBanner}>
-      <div className='container'>
-        <img alt='Dashify 로고' src='./img/logo.svg' className={styles.logo} />
-        <h1 className='hero__title'>{siteConfig.title}</h1>
-        <p className='hero__subtitle'>{siteConfig.tagline}</p>
+      <div className="container">
+        <img alt="Dashify 로고" src="./img/logo.svg" className={styles.logo} />
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className='button button--lg' to='/docs/intro'>
+          <Link className="button button--lg" to="/docs/intro">
             Dashify 사용법 알아보기
-          </Link>
-        </div>
-        <br />
-        <div className={`${styles.buttons} ${styles.downloadSection}`}>
-          <Link className='button button--lg' to='#'>
-            Dashify Server Launcher 다운로드
-          </Link>
-          <Link className='button button--lg' to='#'>
-            Dashify Client 다운로드
           </Link>
         </div>
       </div>
@@ -33,14 +24,20 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title} Docs`}
-      description='Description will go into a meta tag in <head />'
-    >
+    <Layout description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main></main>
+      <hr className={`${styles.divider}`} />
+      <main className={`${styles.rightSection}`}>
+        <div className={`${styles.buttons} ${styles.downloadSection}`}>
+          <Link className="button button--lg" to="#">
+            Launcher 다운로드
+          </Link>
+          <Link className="button button--lg" to="#">
+            Client 다운로드
+          </Link>
+        </div>
+      </main>
     </Layout>
   );
 }
