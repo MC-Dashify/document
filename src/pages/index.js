732c1c0d963dfb2e0e05898169b4fa443d959247
다이츠로 <dytrodev@gmail.com>
2023-07-27 22:03:02 +0900

    :pencil2: fix button content
    Co-Authored-By: aroxu <30624945+aroxu@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,21 @@
-import React from "react";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
+import React from 'react';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
 
-import styles from "./index.module.css";
+import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={styles.heroBanner}>
-      <div className="container">
-        <img alt="Dashify 로고" src="./img/logo.svg" className={styles.logo} />
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      <div className='container'>
+        <img alt='Dashify 로고' src='./img/logo.svg' className={styles.logo} />
+        <h1 className='hero__title'>{siteConfig.title}</h1>
+        <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--lg" to="/docs/intro">
-            Dashify 사용법 알아보기
+          <Link className='button button--lg' to='/docs/intro'>
+            Dashify에 대해 알아보기
           </Link>
         </div>
       </div>
@@ -25,17 +25,17 @@ function HomepageHeader() {
 
 export default function Home() {
   return (
-    <Layout description="Description will go into a meta tag in <head />">
+    <Layout description='Description will go into a meta tag in <head />'>
       <HomepageHeader />
       <hr className={`${styles.divider}`} />
       <main className={`${styles.rightSection}`}>
         <div className={styles.downloadSection}>
           <h1 className={styles.downloadHeader}>다운로드</h1>
           <div className={styles.buttons}>
-            <Link className="button button--lg" to="#">
+            <Link className='button button--lg' to='#'>
               Launcher 다운로드
             </Link>
-            <Link className="button button--lg" to="#">
+            <Link className='button button--lg' to='#'>
               Client 다운로드
             </Link>
           </div>
